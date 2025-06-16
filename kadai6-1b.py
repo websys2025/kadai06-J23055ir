d3ebd6d5f3306b86730bf334abd9f0f746cb9774
j23055 岩崎瑠依 <j23055ir@edu.tuis.ac.jp>
2025-06-16 12:25:08 +0900

    add : kadai6-1のファイルを追加

---
+++ kadai6-1b.py
@@ -0,0 +1,73 @@
+#使い方
+#APIリクエストを送信し、JSON形式のデータを取得
+#全国の旅客人の部分のみを抽出
+#抽出取得したあデータをpandasのDataFrameに変換
+#メタ情報を利用し、数値を名称に変換
+
+import requests
+import pandas as pd
+
+APP_ID = "f115604c2cffd332562ba2c788990d649c9d1d4e"
+API_URL  = "https://api.e-stat.go.jp/rest/3.0/app/json/getStatsData"#エンドポイント
+
+params = {
+    "appId": APP_ID,
+    "statsDataId":"0003440002",#年度別の全国の旅客人（単位：キロ）
+    "cdArea":"00000",
+    "cdCat01": "100",
+    "metaGetFlg":"Y",
+    "cntGetFlg":"N",
+    "explanationGetFlg":"Y",
+    "annotationGetFlg":"Y",
+    "sectionHeaderFlg":"1",
+    "replaceSpChars":"0",
+    "lang": "J"  # 日本語を指定
+}
+
+response = requests.get(API_URL, params=params)
+# Process the response
+data = response.json()
+
+# 統計データからデータ部取得
+values = data['GET_STATS_DATA']['STATISTICAL_DATA']['DATA_INF']['VALUE']
+
+# JSONからDataFrameを作成
+df = pd.DataFrame(values)
+
+# メタ情報取得
+meta_info = data['GET_STATS_DATA']['STATISTICAL_DATA']['CLASS_INF']['CLASS_OBJ']
+
+# 統計データのカテゴリ要素をID(数字の羅列)から、意味のある名称に変更する
+for class_obj in meta_info:
+
+    # メタ情報の「@id」の先頭に'@'を付与した文字列が、統計データの列名と対応している
+    column_name = '@' + class_obj['@id']
+
+    # 統計データの列名を「@code」から「@name」に置換するディクショナリを作成
+    id_to_name_dict = {}
+    if isinstance(class_obj['CLASS'], list):
+        for obj in class_obj['CLASS']:
+            id_to_name_dict[obj['@code']] = obj['@name']
+    else:
+        id_to_name_dict[class_obj['CLASS']['@code']] = class_obj['CLASS']['@name']
+
+    # ディクショナリを用いて、指定した列の要素を置換
+    df[column_name] = df[column_name].replace(id_to_name_dict)
+
+# 統計データの列名を変換するためのディクショナリを作成
+col_replace_dict = {'@unit': '単位', '$': '値'}
+for class_obj in meta_info:
+    org_col = '@' + class_obj['@id']
+    new_col = class_obj['@name']
+    col_replace_dict[org_col] = new_col
+
+# ディクショナリに従って、列名を置換する
+new_columns = []
+for col in df.columns:
+    if col in col_replace_dict:
+        new_columns.append(col_replace_dict[col])
+    else:
+        new_columns.append(col)
+
+df.columns = new_columns
+print(df)
